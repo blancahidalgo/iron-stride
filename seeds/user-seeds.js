abd0e22e1eac7735d53d6dfc1adb77af0c4df03d
Ryan Golder <rgolder78@gmail.com>
2023-05-16 20:31:10 +1000

    Create user-seeds.js file and add first user

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,1 +1,14 @@
 const { User } = require('../models');
+
+const userData = [
+    {
+        first_name: 'Ryan',
+        last_name: 'Golder',
+        email: 'rgolder78@gmail.com',
+        password: 'password123',
+    },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
