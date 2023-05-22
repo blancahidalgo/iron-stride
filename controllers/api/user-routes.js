ed4e7cb07e974a849612af25d6ee207632378b91
blancahidalgo <b.hidalgo.ley@gmail.com>
2023-05-22 19:57:42 +1000

    added code to the user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,2 +1,50 @@
 const router = require("express").Router();
 const { User } = require("../../models");
+
+router.post('/login', async (req, res) => {
+  try {
+    // running an await function - requesting to find a single user from the data base with a specific email
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // This validates the password
+    const validPassword = await userData.checkPassword(req.body.password);
+    // if wrong password then 400 error
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // This saves the session if user id and password match
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// This will log the user out - when the user selects 'logout'
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
