ebafe1546ad15dccc069ddfac5ee1472e5078f4d
blancahidalgo <b.hidalgo.ley@gmail.com>
2023-05-25 22:08:53 +1000

    home-routes update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,32 +1,49 @@
 const router = require('express').Router();
-
+const { Gallery, Painting } = require('../models');
+// TODO: Import the custom middleware
 const withAuth = require('../utils/auth')
-
+// GET all galleries for homepage
 
 
 
 router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
-  console.log('login in homeroute');
   res.render('login');
 });
 
-router.get('/signUp', (req,res) =>{
-  res.render('signup');
-})
-
-
-router.get('/', (req,res) =>{
-  if (!req.session.logged_in){
-    res.redirect('/login');
-    console.log('redirected to /login')
-    return;
-  }
+router.get('/', (req, res) => {
   res.render('profile')
-})
+  //console.log
 
+router.get('/user/:id', async (req, res) => {
+    try {
+        const dbUserData = await User.findById(req.params.id, {
+            include: [
+                {
+                    model: Workout,
+                    attributes: ['name', 'time', 'distance']
+                },
+            ],
+            exclude: [
+                {
+                    attributes: ['password'],
+                },
+            ],
+        });
+        const users = dbUserData.map((User) =>
+        User.get({plain: true})
+        );
+        res.render('profile', {
+            users,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+  });
+});
 
 module.exports = router;
