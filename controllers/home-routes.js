6b65f07d49b6579c337a61837b0670e68a62ced1
Ryan Golder <rgolder78@gmail.com>
2023-05-28 10:27:03 +1000

    getting activities page working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,40 @@
 const router = require('express').Router();
-const { Gallery, Painting } = require('../models');
 // TODO: Import the custom middleware
-const withAuth = require('../utils/auth')
+// const withAuth = require('../utils/auth')
 // GET all galleries for homepage
+const { Workout, User, Category  } = require('../models');
 
 
-
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect('/profile');
     return;
   }
 
   res.render('login');
 });
+
+router.get('/profile', (req, res) => {
+  
+
+  res.render('profile');
+});
+
+// For the all activities page
+router.get("/workout", async (req, res) => {
+  try {
+    const workoutData = await Workout.findAll({
+      include: [{ model: User }, { model: Category }],
+      // exclude
+    });
+
+    const workouts = workoutData.map((workout) => workout.get({ plain: true }));
+
+    res.render("activities-page", { workouts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  });
+
 
 module.exports = router;
