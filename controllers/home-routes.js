1462561fcbc3dc248661ddad0421f85355a1877e
blancahidalgo <b.hidalgo.ley@gmail.com>
2023-05-28 19:56:44 +1000

    fixed logged_in in home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,7 @@ const withAuth = require('../utils/auth')
 
 
 router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect('/');
     return;
   }
