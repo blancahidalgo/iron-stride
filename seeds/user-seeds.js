87d555ee80211e46a4e4b0cdf875a5861bdb8705
Ryan Golder <rgolder78@gmail.com>
2023-05-29 19:39:53 +1000

    Add in more workouts in the db and add profiles for our group

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -5,9 +5,32 @@ const userData = [
         first_name: 'Ryan',
         last_name: 'Golder',
         email: 'rgolder78@gmail.com',
-        password: 'password123',
+        password: 'Password123!',
     },
-    
+    {
+        first_name: 'Blanca',
+        last_name: 'Hidalgo',
+        email: 'b.hidalgo.ley@gmail.com',
+        password: 'Password123!',
+    },
+    {
+        first_name: 'Grethel',
+        last_name: 'Reyes',
+        email: 'reyes.grethelmaec@gmail.com',
+        password: 'Password123!',
+    },
+    {
+        first_name: 'Jeremy',
+        last_name: 'Decherat',
+        email: 'jejedech.63@gmail.com',
+        password: 'Password123!',
+    },
+    {
+        first_name: 'Jeison',
+        last_name: 'Fajardo',
+        email: 'jfajardobolivar@gmail.com',
+        password: 'Password123!',
+    },
 ];
 
 const seedUsers = () => User.bulkCreate(userData);
